a0ee8b55fb4b1a32d7951a243a023bd97aba9551
mudit2812 <muditpandey2812@gmail.com>
2022-11-10 13:42:38 -0800

    Reverted changes to sparse_hamiltonian

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -260,28 +260,6 @@ class TestSparse:
                     ]
                 ),
             ),
-            (
-                [0.5, 0.2, 2],
-                [
-                    qml.GellMannObs(1, wires=0) @ qml.GellMannObs(4, wires=1),
-                    qml.GellMannObs(2, wires=1),
-                    qml.GellMannObs(3, wires=0),
-                ],
-                None,
-                np.array(
-                    [
-                        [2, -0.2j, 0, 0, 0, 0.5, 0, 0, 0],
-                        [0.2j, 2, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 2, 0.5, 0, 0, 0, 0, 0],
-                        [0, 0, 0.5, -2, -0.2j, 0, 0, 0, 0],
-                        [0, 0, 0, 0.2j, -2, 0, 0, 0, 0],
-                        [0.5, 0, 0, 0, 0, -2, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, -0.2j, 0],
-                        [0, 0, 0, 0, 0, 0, 0.2j, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    ]
-                ),
-            ),
         ],
     )
     def test_sparse_matrix(self, coeffs, obs, wires, ref_matrix):
